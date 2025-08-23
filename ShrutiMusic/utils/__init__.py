5c429e26cee9983a5d4fedd1f756590bc4c97194
Haruka x Music 🎶 <deepakkumar54dk16@gmail.com>
2025-08-23 13:33:15 +0530

    Update __init__.py

--- ShrutiMusic/utils/__init__.py
+++ ShrutiMusic/utils/__init__.py
@@ -1,25 +1,3 @@
-# Copyright (c) 2025 Nand Yaduwanshi <NoxxOP>
-# Location: Supaul, Bihar
-#
-# All rights reserved.
-#
-# This code is the intellectual property of Nand Yaduwanshi.
-# You are not allowed to copy, modify, redistribute, or use this
-# code for commercial or personal projects without explicit permission.
-#
-# Allowed:
-# - Forking for personal learning
-# - Submitting improvements via pull requests
-#
-# Not Allowed:
-# - Claiming this code as your own
-# - Re-uploading without credit or permission
-# - Selling or using commercially
-#
-# Contact for permissions:
-# Email: badboy809075@gmail.com
-
-
 from .channelplay import *
 from .database import *
 from .decorators import *
@@ -32,13 +10,12 @@ from .error import *
 from .couple import *
 
 
-# ©️ Copyright Reserved - @NoxxOP  Nand Yaduwanshi
+# ©️ Copyright Reserved - /HARUKA06/Jawanon_ka_music
 
 # ===========================================
-# ©️ 2025 Nand Yaduwanshi (aka @NoxxOP)
-# 🔗 GitHub : https://github.com/NoxxOP/ShrutiMusic
+# 🔗 GitHub : https://github.com/HARUKA06/Jawanon_ka_music
 # 📢 Telegram Channel : https://t.me/ShrutiBots
 # ===========================================
 
 
-# ❤️ Love From ShrutiBots 
+# ❤️ Love From Jawanon_ka_music
